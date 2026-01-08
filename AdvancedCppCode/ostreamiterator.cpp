dfd7e49ddd3a3173da5c860b3b1dbfdd28b4b2c7
JhiWhoon Kang <kangjw0914@naver.com>
2026-01-08 21:51:20 +0900

    STL Ostream Iterator 구현 2026/01/08

--- AdvancedCppCode/ostreamiterator.cpp
+++ AdvancedCppCode/ostreamiterator.cpp
@@ -3,6 +3,49 @@
 #include <list>
 #include <fstream>
 
+template<typename T, typename CharT=char, typename Traits=std::char_traits<CharT>>
+class eostream_iterator
+{
+	//std::ostream* stream;	// 실제로는 ostream 클래스는 아니고 basic_stream이 맞다.
+	std::basic_ostream<CharT,Traits>* stream;	// char만 받으면 안되니까 template 만들기
+	const CharT* delimiter;
+public:
+
+	using iterator_category = std::output_iterator_tag;
+	using value_type = void;
+	using pointer = void;
+	using reference = void;
+	using difference_type = void;
+
+	using char_type = CharT;
+	using traits_type = Traits;
+	using ostream_type = std::basic_ostream<CharT, Traits>;
+
+	eostream_iterator(std::ostream& os, const CharT* const deli = 0) :stream(&os), delimiter(deli) {}
+
+	eostream_iterator& operator*()
+	{
+		return *this;
+	}
+	eostream_iterator& operator++()
+	{
+		return *this;
+	}
+	eostream_iterator& operator++(int)
+	{
+		return *this;
+	}
+	eostream_iterator& operator=(const T& v)
+	{
+		*stream << v;
+		if (delimiter != 0)
+		{
+			*stream << delimiter;
+		}
+		return *this;
+	}
+};
+
 int main(void)
 {
 #pragma region ostream cout stream 출력
@@ -22,7 +65,7 @@ int main(void)
 #pragma endregion
 
 #pragma region ostream file 출력
-#if 01
+#if 0
 	//std::ostream_iterator<int>p(std::cout, ", ");
 	std::ostream_iterator<int>p(std::cout);	// 2번째 인자 없으면, NULL 문자로 구분
 											// 출력: 2030
@@ -35,6 +78,11 @@ int main(void)
 #endif
 #pragma endregion
 
+#pragma region ostream iterator 구현
+	//eostream_iterator<int>p(std::cout, ",");
+	eostream_iterator<int>p(std::cout, ",");
+	*p = 10;	// (p.operator*()).operator=(10);
+#pragma endregion
 
 	return 0;
 }
